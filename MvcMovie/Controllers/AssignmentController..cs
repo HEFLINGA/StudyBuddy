acc78cb9860a7c1df63644e03434ccad01cc5a81
Alexander Heiberg <ajheiberg@coastal.edu>
2021-10-02 14:51:08 -0400

    first view

--- MvcMovie/Controllers/AssignmentController..cs
+++ MvcMovie/Controllers/AssignmentController..cs
@@ -8,19 +8,18 @@ namespace MvcMovie.Controllers
         // 
         // GET: /Assignment/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        // 
-        // GET: /Assignment/Welcome/ 
-
-        // GET: /HelloWorld/Welcome/ 
-        // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID = 1)
+        
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
