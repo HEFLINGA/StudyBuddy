67c25e88f34e55df52acd5c760577127d149353e
Alexander Heiberg <ajheiberg@coastal.edu>
2021-10-05 19:14:45 -0400

    Model changes

---
+++ MvcMovie/Controllers/AssignmentController.cs
@@ -0,0 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public AssignmentController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Assignment
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Assignment.ToListAsync());
+        }
+
+        // GET: Assignment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assignment = await _context.Assignment
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assignment);
+        }
+
+        // GET: Assignment/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Assignment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Type,Title,DueDate,Points,Description")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(assignment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(assignment);
+        }
+
+        // GET: Assignment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assignment = await _context.Assignment.FindAsync(id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+            return View(assignment);
+        }
+
+        // POST: Assignment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,Title,DueDate,Points,Description")] Assignment assignment)
+        {
+            if (id != assignment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(assignment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AssignmentExists(assignment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(assignment);
+        }
+
+        // GET: Assignment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var assignment = await _context.Assignment
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return View(assignment);
+        }
+
+        // POST: Assignment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var assignment = await _context.Assignment.FindAsync(id);
+            _context.Assignment.Remove(assignment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AssignmentExists(int id)
+        {
+            return _context.Assignment.Any(e => e.Id == id);
+        }
+    }
+}
--- MvcMovie/Models/Assignment.cs
+++ MvcMovie/Models/Assignment.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
-    public class Assignments
+    public class Assignment
     {
         public int Id { get; set; }
         public string Type { get; set; }
