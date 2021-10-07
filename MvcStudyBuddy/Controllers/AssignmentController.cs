9013784688b4696f72f4ef4404ada1d854a9942d
Alexander Heiberg <ajheiberg@coastal.edu>
2021-10-07 18:25:27 -0400

    Rebuild of MVC model

---
+++ MvcStudyBuddy/Controllers/AssignmentController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcStudyBuddy.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        } 
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
