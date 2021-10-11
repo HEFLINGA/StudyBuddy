3bb296608532d5700fee02e7089327914de76cbf
Tanner <tmambrosi@coastal.edu>
2021-10-11 17:48:00 -0400

    testing edit funstionality

--- MvcLogin/Models/Account.cs
+++ MvcLogin/Models/Account.cs
@@ -1,5 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace MvcLogin.Models
 {
@@ -8,6 +10,7 @@ namespace MvcLogin.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        [Display(Name = "Date of Birth")]
         [DataType(DataType.Date)]
         public DateTime Dob { get; set; }
         public string Email { get; set; }
