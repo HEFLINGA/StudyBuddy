38b609b3ff46f96841cb52eaed6ee69f53bb62d8
Tanner <tmambrosi@coastal.edu>
2021-10-11 18:50:48 -0400

    Added Input Validation

--- MvcLogin/Models/Account.cs
+++ MvcLogin/Models/Account.cs
@@ -8,12 +8,23 @@ namespace MvcLogin.Models
     public class Account
     {
         public int Id { get; set; }
-        public string Name { get; set; }
 
+        
+        [Required]
+        [StringLength(250, MinimumLength = 2)]
+        public string Name { get; set; }
+        
+        [Required]
         [Display(Name = "Date of Birth")]
         [DataType(DataType.Date)]
         public DateTime Dob { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength = 3)]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(24, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
